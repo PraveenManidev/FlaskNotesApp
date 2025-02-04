f8f9b62e66e94b0dcfd795dca88a818321201565
PraveenManidev <praveenthuyamani@gmail.com>
2025-02-04 21:58:53 +0530

    Authentication login and signup

--- website/auth.py
+++ website/auth.py
@@ -6,8 +6,18 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form 
-    print(data)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password') 
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password,password):
+                flash('Logged in successfully', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Invalid password, Try again', category='error')
+        else:
+            flash('Invalid user',category='error')
     return render_template("login.html")
 
 @auth.route('/logout')
@@ -21,8 +31,10 @@ def signup():
         first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists',category='error')
+        elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
         elif len(first_name) < 2:
             flash('First name should be greater than 2 character', category='error')
